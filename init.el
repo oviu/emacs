77d213c30422b75f0faca2abb5b6674983a13170
oviu <oviu@protonmail.com>
2020-11-15 05:09:48 -0500

    stuff

--- init.el
+++ init.el
@@ -28,12 +28,18 @@
 (show-paren-mode 1)
 (setq show-paren-delay 0)
 
-(global-set-key (kbd "TAB") 'self-insert-command)			    
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 4)
 (setq indent-line-function 'insert-tab)
 (defalias 'yes-or-no-p 'y-or-n-p)
+
+(global-set-key (kbd "TAB") 'self-insert-command)
 (global-set-key (kbd "<SPC>") 'legerity-space)
+;; # (global-set-key (kbd "<backspace>") 'legerity-backspace)
+(global-set-key (kbd "S-<backspace>") 'legerity-del-spaces)
+(add-hook 'python-mode-hook
+          (lambda () (local-set-key (kbd "<backspace>") 'legerity-backspace)
+		  			 (local-set-key (kbd "<tab>") 'legerity-placeholder)))
 
 (setq make-backup-files nil)
 (setq create-lockfiles nil)
@@ -69,11 +75,10 @@
  ;; (set-foreground-color "#676767")
  ;; (set-face-foreground 'font-lock-comment-face "#d98695")
  ;; (set-face-foreground 'font-lock-string-face "#676767")
- ;; (set-face-foreground 'font-lock-type-face "#676767") 
- ;; (set-face-foreground 'font-lock-variable-name-face "#676767") 
- ;; (set-face-foreground 'font-lock-function-name-face "#676767") 
+ ;; (set-face-foreground 'font-lock-type-face "#676767")
+ ;; (set-face-foreground 'font-lock-variable-name-face "#676767")
+ ;; (set-face-foreground 'font-lock-function-name-face "#676767")
 ;; (set-face-foreground 'font-lock-keyword-face "#431C53")
-
 
 
 ;; my old Eclipse Java theme
@@ -82,10 +87,10 @@
  (set-frame-font "Roboto Mono 12" nil t)
  (set-background-color "#2E3436") ;; #FFFAFA
  (set-foreground-color "#D3D7CF")
- (set-face-foreground 'font-lock-comment-face "#818FA6") 
+ (set-face-foreground 'font-lock-comment-face "#818FA6")
  (set-face-foreground 'font-lock-string-face "#FFE5B4")
- (set-face-foreground 'font-lock-type-face "#676767") 
- (set-face-foreground 'font-lock-variable-name-face "#D3D7CF") 
+ (set-face-foreground 'font-lock-type-face "#676767")
+ (set-face-foreground 'font-lock-variable-name-face "#D3D7CF")
  (set-face-foreground 'font-lock-function-name-face "#D3D7CF")
  (set-face-foreground 'font-lock-keyword-face "#D197D9")
 (set-face-background 'fringe "black")
@@ -129,6 +134,7 @@
 	(,(kbd "d") . ido-switch-buffer)
 	(,(kbd "T") . beginning-of-buffer)
 	(,(kbd "Y") . end-of-buffer)
+	(,(kbd "/") . legerity-test)
     )
  ;; Make mode global rather than buffer local
    :global 1
@@ -145,7 +151,7 @@
 	     (kill-sexp))
 	(t (progn (forward-word) (backward-kill-word 1))))
 )
-
+   
 (defun legerity-copy ()
 	(interactive)
 	(cond
@@ -224,8 +230,22 @@
 	   (insert "\s")
 ))
 
-;; need function to remove 4 spaces   	
+(defun legerity-backspace ()
+(interactive)
+	(if (and (eq (char-before) ?\s) (eq (char-before (- (point) 1)) ?\s)
+	(eq (char-before (- (point) 2)) ?\s) (eq (char-before (- (point) 3)) ?\s))
+	(delete-backward-char 4)
+	(delete-backward-char 1)))
 
+(defun legerity-del-spaces ()
+(interactive)
+(delete-char 4))
+
+;; move to first non-whitespace character on current line
+(defun legerity-python-tab ()
+(interactive)
+)
+	    
 (defun legerity-quit ()
        (interactive)
 	(if (active-minibuffer-window)
@@ -248,6 +268,11 @@
 (interactive)
 (message "not implemented yet!")
 )
+
+;; used for testing emacs lisp code
+(defun legerity-test ()
+(interactive)
+(backward-delete-char 4))
 
 ;; for quick/small python tests/scripts
 (defun python-buffer-run ()
@@ -279,5 +304,3 @@
 (org-babel-do-load-languages
  'org-babel-load-languages '((python . t)))
 (setq org-confirm-babel-evaluate nil)
-
-
